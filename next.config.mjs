ed151cd690c163c4106ebc294c84557b68a87fb9
Muhammad Maaz Uddin <maazawan100@gmail.com>
2025-09-10 21:35:27 +0500

    Update basePath for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,8 @@ const nextConfig = {
   },
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/html-cleanser-tool' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/html-cleanser-tool/' : '',
 }
 
 export default nextConfig
